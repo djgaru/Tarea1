9ae26f7d5e3cd04dd6e0fd2f4e94780a63ac5b71
djgaru <djehernandez@gmail.com>
2014-09-29 19:02:14 -0600

    Tarea 1

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	//Edgar Alfonso Pérez Hernández
+	//Carnet: 201021195
+	
+	public static void main(String[] args) {
+
+			Scanner x =	 new Scanner(System.in);
+
+		int numero;
+
+			System.out.println("Ingrese un numero: ");
+			numero = x.nextInt();
+
+		int cont;
+
+			for ( cont = 0; cont <= numero; ++cont ) {
+				if ((cont%2) == 0) {
+			System.out.println("Si es un numero primo "+cont);
+			} else {
+			System.out.println("No es un numero primo "+cont);
+			}
+		}
+	}
+}   
